9d0f883188b0304718e8d2a34e022aa84b4b1be2
Tephirax <t_seniah@hotmail.com>
2023-04-07 10:11:35 +0100

    Added Mod options

--- Invert-Look-Controls/Invert-Look-Controls.cs
+++ Invert-Look-Controls/Invert-Look-Controls.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using OWML.Common;
 using OWML.ModHelper;
 using System.Reflection;
 using UnityEngine;
@@ -8,6 +9,8 @@ namespace InvertLookControls
     public class InvertLookControls : ModBehaviour
     {
         public static InvertLookControls Instance;
+        public bool toggleShipX;
+        public bool toggleShipY;
 
         private void Awake()
         {
@@ -18,8 +21,23 @@ namespace InvertLookControls
             Instance = this;
         }
 
+        public void Start()
+        {
+            toggleShipX = ModHelper.Config.GetSettingsValue<bool>("Toggle Ship X");
+            toggleShipY = ModHelper.Config.GetSettingsValue<bool>("Toggle Ship Y");
+        }
+
+        public override void Configure(IModConfig config)
+        {
+            toggleShipX = ModHelper.Config.GetSettingsValue<bool>("Toggle Ship X");
+            ModHelper.Console.WriteLine("Toggle Ship X setting changed.");
+            toggleShipY = ModHelper.Config.GetSettingsValue<bool>("Toggle Ship Y");
+            ModHelper.Console.WriteLine("Toggle Ship Y setting changed.");
+        }
     }
+
     
+
     [HarmonyPatch]
     public class UpdateInversionPatch
     {
@@ -28,8 +46,13 @@ namespace InvertLookControls
         [HarmonyPatch(typeof(BaseInputManager), nameof(BaseInputManager.UpdateInversion))]
         public static bool BaseInputManager_UpdateInversion_Prefix()
         {
-            InputLibrary.pitch.InversionFactor = PlayerData.GetShipLookInversionFactor();
-            InputLibrary.yaw.InversionFactor = PlayerData.GetShipLookInversionFactor();
+            if (InvertLookControls.Instance.toggleShipY)
+                InputLibrary.pitch.InversionFactor = PlayerData.GetShipLookInversionFactor();
+            else InputLibrary.pitch.InversionFactor = PlayerData.GetShipLookInversionFactor() * -1;
+            if (InvertLookControls.Instance.toggleShipX) 
+                InputLibrary.yaw.InversionFactor = PlayerData.GetShipLookInversionFactor();
+            else
+                InputLibrary.yaw.InversionFactor = PlayerData.GetShipLookInversionFactor() * -1;
             InputLibrary.look.InversionFactor = PlayerData.GetShipLookInversionFactor();
             InputLibrary.freeLook.InversionFactor = PlayerData.GetShipLookInversionFactor();
             return false;
@@ -41,7 +64,7 @@ namespace InvertLookControls
         {
             float num = 1f;
             num *= __instance._playerCam.fieldOfView / __instance._initFOV;
-            float num2 = OWInput.GetAxisValue(InputLibrary.look, InputMode.Character | InputMode.ScopeZoom | InputMode.NomaiRemoteCam).x * num * -1;
+            float num2 = OWInput.GetAxisValue(InputLibrary.look, InputMode.Character | InputMode.ScopeZoom | InputMode.NomaiRemoteCam).x * num * -1f;
             __instance._lastTurnInput = num2;
             bool flag = Locator.GetAlarmSequenceController() != null && Locator.GetAlarmSequenceController().IsAlarmWakingPlayer();
             float num3 = ((__instance._signalscopeZoom || flag) ? (PlayerCameraController.LOOK_RATE * PlayerCameraController.ZOOM_SCALAR) : PlayerCameraController.LOOK_RATE);
